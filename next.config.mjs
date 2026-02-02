e3946db18fcce3c463458c09bf6b2d953d060925
Nico <judesnicolas@gmail.com>
2026-02-02 12:28:58 -0400

    PATCH : confit date worker and pdfjs

--- next.config.mjs
+++ next.config.mjs
@@ -38,7 +38,7 @@ const nextConfig = {
             },
             {
               key: 'Content-Security-Policy',
-              value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self' https:; frame-ancestors 'none';"
+              value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self' blob: https:; worker-src 'self' blob:; frame-ancestors 'none';"
             }
           ]
         }
